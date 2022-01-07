ab6102a65825b50ef28792ce3bedf6f611ed9067
wangziyuan123 <wangziyuan.1@bytedance.com>
2022-01-07 16:50:46 +0800

    first

---
+++ src/test/java/com/example/learngit/LearngitApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.learngit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearngitApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("这是测试！");
+    }
+
+}
